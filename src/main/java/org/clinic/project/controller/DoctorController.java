f14b461918ddadfe3aefbe014a970b42688ee7df
MattReda11 <103293959+MattReda11@users.noreply.github.com>
2022-11-03 13:44:18 -0400

    Update DoctorController.java

--- src/main/java/org/clinic/project/controller/DoctorController.java
+++ src/main/java/org/clinic/project/controller/DoctorController.java
@@ -8,12 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.clinic.project.model.Doctor;
-import org.clinic.project.model.HealthTicket;
+// import org.clinic.project.model.HealthTicket;
 import org.clinic.project.service.DoctorService;
-import org.clinic.project.service.HealthTicketService;
+// import org.clinic.project.service.HealthTicketService;
 
 @Controller
 public class DoctorController {
@@ -21,27 +22,34 @@ public class DoctorController {
     @Autowired
     private DoctorService doctorService;
 
-    @Autowired
-    private HealthTicketService healthTicketService;
+    // @Autowired
+    // private HealthTicketService healthTicketService;
+    
+ /*-------------------------- DOCTOR REGISTRATION --------------------------*/
+	/*---- REGISTER DOCTOR ----*/
+	@RequestMapping("/doctor_register")
+	public String showDoctorRegistrationForm(Model model) {
+		model.addAttribute("doctor", new Doctor());
+		return "doctor_register";
+	}
 
-    @GetMapping("/doctor_register")
-    public String showDoctorRegistrationForm(Model model) {
-        model.addAttribute("doctor", new Doctor());
-        return "doctor_register";
-    }
+	/*---- PROCESS REGISTER ----*/
+	@PostMapping("/process_doctor_register")
+	public String processDoctorRegister(@Valid @ModelAttribute("doctor") Doctor doctor, BindingResult bindingResult) {
 
-    @PostMapping("/process_doctor_register")
-    public String processDoctorRegister(@Valid Doctor doctor, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "patient_register";
+		} else {
+			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			String encodedPassword = passwordEncoder.encode(doctor.getPassword());
+			doctor.setPassword(encodedPassword);
+			doctorService.save(doctor);
+			return "redirect:/doctor/login";
+		}
+	}
 
-        if (bindingResult.hasErrors()) {
-            return "doctor_register";
-        } else {
-            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-            String encodedPassword = passwordEncoder.encode(doctor.getPassword());
-            doctor.setPassword(encodedPassword);
-            doctorService.save(doctor);
-            return "redirect:/login";
-        }
-    }
-
+	@RequestMapping("/doctor/login")
+	public String viewDoctorloginpage() {
+		return "doctor_login";
+	}
 }
