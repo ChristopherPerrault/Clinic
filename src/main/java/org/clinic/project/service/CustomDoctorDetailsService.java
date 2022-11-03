aa030faacfb1130fc65c21c30cb619549471a95c
ChristopherPerrault <christopherperrault@hotmail.com>
2022-11-03 09:42:22 -0400

    added custom services patients/doctors

---
+++ src/main/java/org/clinic/project/service/CustomDoctorDetailsService.java
@@ -0,0 +1,25 @@
+package org.clinic.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.clinic.project.dao.DoctorRepository;
+import org.clinic.project.model.Doctor;
+import org.clinic.project.security.CustomDoctorDetails;
+
+public class CustomDoctorDetailsService implements UserDetailsService {
+
+    @Autowired
+    private DoctorRepository doctorRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Doctor doctor = doctorRepo.findByDoctorID(username);
+        if (doctor == null) {
+            throw new UsernameNotFoundException("Doctor not found");
+        }
+        return new CustomDoctorDetails(doctor);
+    }
+}
---
+++ src/main/java/org/clinic/project/service/CustomPatientDetailsService.java
@@ -0,0 +1,25 @@
+package org.clinic.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.clinic.project.dao.PatientRepository;
+import org.clinic.project.model.Patient;
+import org.clinic.project.security.CustomPatientDetails;
+
+public class CustomPatientDetailsService implements UserDetailsService {
+
+    @Autowired
+    private PatientRepository patientRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Patient patient = patientRepo.findByPatientID(username);
+        if (patient == null) {
+            throw new UsernameNotFoundException("Patient not found");
+        }
+        return new CustomPatientDetails(patient);
+    }
+}
